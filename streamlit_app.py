bc7066dfd37089417fee38cd2eb278a11e047e43
Tiago Sampaio <67651223+samsystems-source@users.noreply.github.com>
2024-07-31 23:49:28 -0300

    teste conection

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ st.write('The name on your Smoothie will be:', name_on_order)
 
 
 #session = get_active_session()
-cnx = st.connection("snowflake")
+cnx = st.connection(**st.secrets.db_credentials)
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"))
 # st.dataframe(data=my_dataframe, use_container_width=True)
